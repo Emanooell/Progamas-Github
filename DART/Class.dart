7cf486450884565be653fe90d622fad0855e19ca
Emanoel Rodrigues <emanuellegario@gmail.com>
2023-08-03 21:24:31 -0300

    Adicionando meu primeiro progama usando uma Classe

--- DART/Class.dart
+++ DART/Class.dart
@@ -1,11 +1,27 @@
 void main(){
-class Frutas{
+String nome = "Laranja";
+double peso = 100.0;
+String cor = "Verde e Amarela";
+String sabor = "Doce e cítrica";
+int diasDeColheita = 25;
+bool isMadura;
+
+Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasDeColheita);
+Fruta fruta02 = Fruta("Uva", 40, "Roxa", "Doce", 20);
+
+print(fruta01.sabor);
+
+}
+
+class Fruta{
   String nome;
   double peso;
   String cor;
   String sabor;
   int diasDeColheita;
-  bool? ismadura;
-}
-}
+  bool? isMadura;
+ Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDeColheita, {this.isMadura});
 
+
+
+}
